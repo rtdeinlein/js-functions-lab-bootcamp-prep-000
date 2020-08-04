4c53879b951e63ae443b44dd000faa15ffe0bb74
Reid T Deinlein <rtdeinlein5@gmail.com>
2020-08-04 15:37:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var date = "July 3rd"
 console.log('My birthday is ${date}')
 function happyHolidays(){return "Happy Holidays!"}
-function happyHolidaysto(){return "Happy Holidays Kyrsten!"}
+function happyHolidaysto(Kyrsten){return ${Happy Holidays Kyrsten
