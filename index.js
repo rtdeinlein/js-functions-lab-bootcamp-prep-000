6b85f5479e16141bb24c322998374c6ad7edda59
Reid T Deinlein <rtdeinlein5@gmail.com>
2020-08-04 16:00:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function happyHolidays(){
   return "Happy holidays!"}
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`}
-function happyHolidayTo(holiday, name){
+function happyCustomHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`}
 function holidayCountdown(holiday,days){
   return `It\'s ${days} days until ${holiday}!`}
