c0dc4c240726408e7fffef33cc0abc7979be7214
Reid T Deinlein <rtdeinlein5@gmail.com>
2020-08-04 15:59:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ console.log('My birthday is ${date}')
 function happyHolidays(){
   return "Happy holidays!"}
 function happyHolidaysTo(name) {
-  return "Happy holidays, ${name}!"}
+  return `Happy holidays, ${name}!`}
 function happyHolidayTo(holiday, name){
-  return "Happy ${holiday}, ${name}!"}
+  return `Happy ${holiday}, ${name}!`}
 function holidayCountdown(holiday,days){
   return `It\'s ${days} days until ${holiday}!`}
